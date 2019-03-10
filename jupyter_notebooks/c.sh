d689cba91d7998fc1a2b64d9a6d0043b8aaad80f
Bjorn Sandstede <bjorn_sandstede@brown.edu>
2019-03-10 19:44:40 -0400

    Delete c.sh

--- jupyter_notebooks/c.sh
+++
@@ -1,9 +0,0 @@
-parnames={1:'Mu',2:'Nu',3:'H',4:'S',5:'E',11:'Period'}
-NDIM=5, IPS=4, IRS=1, ILP=1
-ICP=['Mu','Period','S','E']
-NTST=35, NCOL=4, IAD=3, ISP=2, ISW=1, IPLT=0, NBC=5, NINT=2
-NMX=500, NPR=0, MXBF=10, IID=3, ITMX=9, ITNW=7, NWTN=3, JAC=0
-EPSL=1e-10, EPSU=1e-10, EPSS=1e-08
-DS=0.01, DSMIN=0.00001, DSMAX=0.01, IADS=1
-THL={}, THU={}
-UZR = {-1:0.18,-1:0.215}
